780c98c716fcbea448d806e4a2aaabf034140577
pablosc <pspablosanchezc@gmail.com>
2021-12-30 18:17:23 -0600

    correccion de script y cambio de fuente

--- scripts/IMC.js
+++ scripts/IMC.js
@@ -11,14 +11,17 @@ function IMCAplicada() {
     const Resultado = document.getElementById("IMCcheck");
     Resultado.innerText = "Tu IMC (Índice de masa corporal) es " + Division.toFixed(2);
     if (Division < 18.5) {
-        const imcBaja = document.getElementById("bajo");
+        const imcBaja = document.getElementById("medio");
         imcBaja.innerText = "Tu IMC se encuentra dentro del rango insuficiente.";
+        imcBaja.style.color = "rgb(128, 184, 230)"
     } else if (Division < 25) {
         const imcMedia = document.getElementById("medio");
         imcMedia.innerText = "Tu IMC se encuentra dentro del rango normal o saludable.";
+        imcMedia.style.color = "black"
     } else {
-        const imcAlta = document.getElementById("alto");
+        const imcAlta = document.getElementById("medio");
         imcAlta.innerText = "Tu IMC se encuentra dentro del rango de sobrepeso.";
+        imcAlta.style.color = "red"
     }
 }
 
